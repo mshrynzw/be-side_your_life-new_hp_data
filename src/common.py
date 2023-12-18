32079cae9234ffbc5eeb86ae1c1b32e2b3a9f53b
kaoruyuki <>
2023-12-18 11:40:02 +0900

    Fix

--- src/common.py
+++ src/common.py
@@ -3,14 +3,14 @@ from datetime import datetime
 
 
 def set_common():
-    with open('./conf/common.json', 'r') as f:
+    with open('../conf/common.json', 'r') as f:
         common_conf = json.load(f)
 
     return common_conf
 
 
 def set_log():
-    with open('./conf/log.json', 'r') as f:
+    with open('../conf/log.json', 'r') as f:
         log_conf = json.load(f)
     log_conf["handlers"]["fileHandler"]["filename"] = './log/{}.log'.format(datetime.utcnow().strftime("%Y%m%d%H%M%S"))
 
